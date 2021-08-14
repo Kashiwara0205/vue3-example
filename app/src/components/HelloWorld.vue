2282439cb63653c21769e5b4fbd35b5aafa43901
kashiwara <tanakatarou0205@gmail.com>
2021-08-14 19:49:01 +0900

    Add vue3 project

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    {{ getMsg }}
+    <br>
+    <input v-model="color" placeholder="edit me">
+    <button v-on:click="onClick">Click me</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, computed, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    msg:{
+      type: String,
+      required: true
+    }
+  },
+  setup(props){
+    const getMsg = computed(() => props.msg)
+
+    const color = ref("red")
+
+    const onClick = ()=>{
+      console.log("click!")
+      console.log(color.value)
+    }
+
+    return{
+      getMsg,
+      color,
+      onClick
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
